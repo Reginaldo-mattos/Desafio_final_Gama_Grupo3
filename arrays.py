c0b8625ff1834b2548fb56c58eed17e89d0b5809
Reginaldo Mattos <82181032+Reginaldo-mattos@users.noreply.github.com>
2024-06-10 11:08:04 -0300

    Criado usando o Colab

---
+++ arrays.py
@@ -0,0 +1,50 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "private_outputs": true,
+      "provenance": [],
+      "authorship_tag": "ABX9TyPaOFyix2hqQrfTH2CGg72L",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Reginaldo-mattos/Desafio_final_Gama_Grupo3/blob/main/arrays.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "pEZw6UKUeKRJ"
+      },
+      "outputs": [],
+      "source": [
+        "# Array de produtos de uma loja de eletrônicos\n",
+        "produtos_eletronicos = [\"smartphone\", \"notebook\", \"fones de ouvido\", \"smartwatch\", \"câmera digital\"]\n",
+        "\n",
+        "# Array de anos de nascimento de familiares e amigos\n",
+        "anos_nascimento = [1990, 1985, 1992, 1988, 1995]\n",
+        "\n",
+        "# Imprimindo os arrays\n",
+        "print(\"Produtos da loja de eletrônicos:\", produtos_eletronicos)\n",
+        "print(\"Anos de nascimento de familiares e amigos:\", anos_nascimento)\n"
+      ]
+    }
+  ]
+}
